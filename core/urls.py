267636e57c5b5f3154244974f18d2d6e2073966c
Kauã Cavalcante <kauasilvacavalcante21@gmail.com>
2025-07-22 23:33:43 -0300

    change: fix initial URL to login.

--- core/urls.py
+++ core/urls.py
@@ -4,11 +4,10 @@ from django.urls import include, path
 from .views import InicialView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', InicialView.as_view(), name='inicial'),
+    path('', include('user.urls')),
+    path('home/', InicialView.as_view(), name='inicial'),
     path('clientes/', include('customer.urls')),
     path('veiculos/', include('vehicles.urls')),
     path('manutencoes/', include('maintenances.urls')),
-    path('usuarios/', include('user.urls')),
-
+    path('admin/', admin.site.urls),
 ]
--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import UserListView, UserRegisterView
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('', views.LoginView.as_view(), name='login'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('users/', UserListView.as_view(), name='users'),
